1cb078285395c301e2aeb8c41bb7bc68af67f9d2
010Harsh010 <harshsingh794613@gmail.com>
2025-01-05 00:44:54 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-const whitelist = ['http://192.168.29.82:5173', 'http://localhost:5173'];
+const whitelist = ['http://localhost:5173','https://mytube-beta-version.vercel.app','https://mytube-beta-version.vercel.app/login','https://mytube-beta-version.vercel.app/','https://mytube-beta-version.vercel.app/setting'];
 
 const corsOptions = {
   origin: function (origin, callback) {
